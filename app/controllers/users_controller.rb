f3360a7ce1c681f9238dbfdec51704935a28d7a0
i3po <chernov.alexe2017@yandex.ru>
2018-08-04 19:19:19 +0300

    fix migration added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,10 +26,10 @@ class UsersController < ApplicationController
     if User.where(email: @user.email)
       @user = nil
     end
-    if @user&.save
+    if @user && @user.save
       render json: @user, status: :created, location: @user
     else
-      render json: @user&.errors, status: :unprocessable_entity
+      render json: @user && @user.errors, status: :unprocessable_entity
     end
   end
 
