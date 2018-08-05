2c55a7b967a480a7d227841c7d8e84ec7170a655
i3po <chernov.alexe2017@yandex.ru>
2018-08-05 21:18:09 +0300

    orders created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_04_120833) do
+ActiveRecord::Schema.define(version: 2018_08_05_163902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2018_08_04_120833) do
     t.integer "user_id"
     t.index ["category_id"], name: "index_category_id"
     t.index ["user_id"], name: "index_user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "client_id"
+    t.bigint "seller_id"
+    t.float "order_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.json "order_items", default: {}
   end
 
   create_table "uploads", force: :cascade do |t|
