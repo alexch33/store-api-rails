05bb42d87bd73657988f94f638e3407863f3d5e2
i3po <chernov.alexe2017@yandex.ru>
2018-08-08 22:40:10 +0300

    small fix

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,17 +5,20 @@ class Order < ApplicationRecord
    self.order_items.map do |item|
      original = Item.where(id: item['item_id']).first
      {
-         id: item['item_id'],
-         title: original[:title],
+         item_id: item['item_id'],
+         item_title: original[:title],
          item_raiting: original[:rating],
-         price: item['price'],
-         quantity: item['quantity'],
+         item_price: item['price'],
+         item_quantity: item['quantity'],
          customer_nick: customer[:nick],
          seller_nick: seller[:nick],
          customer_id: customer[:id],
          seller_id: seller[:id],
-         created_at: original[:created_at],
-         updated_at: original[:updated_at],
+         item_created_at: original[:created_at],
+         item_updated_at: original[:updated_at],
+         order_created_at: self[:created_at],
+         order_updated_at: self[:updated_at],
+         order_id: self[:id],
      }
    end
   end
