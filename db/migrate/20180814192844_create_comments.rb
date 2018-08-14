2a4038171a0d04b12d1a093326b15799865a396f
i3po <chernov.alexe2017@yandex.ru>
2018-08-14 22:30:15 +0300

    created comments model && migration

---
+++ db/migrate/20180814192844_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.string :author
+      t.text :body
+      t.float :rating
+      t.references :item, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
